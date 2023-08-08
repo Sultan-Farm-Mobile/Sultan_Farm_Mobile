3edd9f936d3c9f5764a698505db28c47dfd2d3bd
David <davidarrozaqi085@gmail.com>
2023-08-08 14:01:32 +0700

    Penambahan Folder
    Pemnambahan folder yang tidak ikut ter push dikarenakan tidak ada isinya

---
+++ lib/Models/Contoh_Models.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Contoh_Models extends StatelessWidget {
+  const Contoh_Models({Key? key}) : super(key: key);
+
+//! Kalo Models ini biasanya dipake buat isi nya
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/Widgets/Contoh_Widgets.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class Contoh_Widgets extends StatelessWidget {
+  const Contoh_Widgets({Key? key}) : super(key: key);
+
+//! Widget Ini biasanya dipake buat tampilan card(contoh)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
