1bddcd8320be4b464c1d23bae198176a55ac89b4
David <davidarrozaqi085@gmail.com>
2023-08-08 11:11:57 +0700

    Penyesuaian Folder

---
+++ lib/Theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color blackColor = Color(0xff000000);
+const Color grey2Color = Color(0xffE5E5E5);
+const Color greyColor = Color(0xffAAAAAA);
+const Color yellowColor = Color(0xffEDBE16);
+const Color whiteColor = Color(0xffFFFFFF);
+
+TextStyle poppinsTextStyle = GoogleFonts.mulish();
